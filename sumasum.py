7b5059ddc430f843fcba2ddd5299cdd7a5c29966
Julio1229898 <A01229898@itesm.mx>
2015-02-16 13:38:33 -0600

    #WSQ07

---
+++ sumasum.py
@@ -0,0 +1,9 @@
+print ("Welcome to sumasum ") #Julio Cesar Gonzalez Uribe A01229898
+min= int(input("put the lower number that you want to sum "))
+max= int(input("put the bigger number that you want to sum "))
+a=0
+c=min
+while (min<max):
+  min=min+1
+  c=c+min
+print (c)
