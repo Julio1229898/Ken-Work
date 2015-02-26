44c02f888ab6051fb362cc9d2c32584fe333c8fa
Julio1229898 <A01229898@itesm.mx>
2015-02-26 11:04:38 -0600

    #WSQ09

---
+++ WSQ09.py
@@ -0,0 +1,11 @@
+s="yes" #Julio Cesar Gonzalez Uribe A01229898
+while(s=='yes'):
+ n=int(input("give a number to give you the factorial (a positive number) "))
+ a=1
+ b=a
+ while (a<n):
+  a=a+1 
+  b=b*a
+ print('the answer of ',n,'! is: ',b)  
+ s=str(input("if you want to continue write yes "))
+print ('thanks have a great day') 
